2f51c2a95b2673e74ea61d63610dfbb7340e8d6c
Kenil Patel <kenspatel96@gmail.com>
2017-07-01 11:34:53 +0530

    Update and rename DisjointSet.c to DisjointSet.h

--- DisjointSet.h
+++ DisjointSet.h
@@ -80,21 +80,3 @@ void displayDisjointSet(DS ds){
         printf("\b}\n");
     }
 }
-
-int main(int argc, char* argv[]){
-
-    DS ds = createDisjointSet(8);
-    MergeSet(ds,0,3);
-    MergeSet(ds,1,4);
-    MergeSet(ds,0,4);
-    MergeSet(ds,3,5);
-    MergeSet(ds,7,2);
-    displayDisjointSet(ds);
-    printf("\n");
-    
-    printf("Are friends %d and %d? %d\n",1,5,AreFriends(ds,1,5));
-    printf("Are friends %d and %d? %d\n",2,5,AreFriends(ds,2,5));
-
-    
-    return 0;
-}
